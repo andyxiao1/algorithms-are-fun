f939718169d132218687192ba30fd54b81177408
Andy Xiao <andyxiao@seas.upenn.edu>
2020-03-09 00:46:44 -0400

    Learn C++

--- test/a.cpp
+++ test/a.cpp
@@ -3,12 +3,13 @@ using namespace std;
 int main() {
     int x;
     vector<int> v;
+    int a = 56;
     while (cin >> x) {
         v.push_back(x);
     }
     sort(v.rbegin(), v.rend());
     for (int i : v) {
-        cout << i << "\n";
+        cout << i << "\n" << a;
     }
     return 0;
 }
